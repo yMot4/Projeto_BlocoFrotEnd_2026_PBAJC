deb560893e45f455101ce87191e3818030824e5e
Claudia Santos Silva <claudia.tss@outlook.com>
2026-03-23 17:45:23 -0300

    Retirados comentários

--- app-viagens/src/App.jsx
+++ app-viagens/src/App.jsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
 import Card from "./Card";
 
