543d105643be8d005a1e88d38f1130ded58b2a30
Claudia Santos Silva <claudia.tss@outlook.com>
2026-03-23 17:03:50 -0300

    Imagem, titulo e subtitulo OK

--- app-viagens/src/Card.jsx
+++ app-viagens/src/Card.jsx
@@ -11,35 +11,35 @@ export default function Card({
   valor,
 }) {
   return (
-    <div className={stylesCard.container_master}>
-      <div className={stylesCard.card_container}>
-        <div className={stylesCard.card_image}>
-          <img
-            src={imagem}
-            alt="Imagem do destino"
-            className={stylesCard.image}
-          />
+    <div className={stylesCard.card_container}>
+      <div className={stylesCard.card_image}>
+        <img
+          src={imagem}
+          alt="Imagem do destino"
+          className={stylesCard.image}
+        />
+      </div>
+      <div className={stylesCard.card_info}> 
+        <div className={stylesCard.card_titulo}> 
+          <span className={stylesCard.titulo}>{titulo}</span>
+        </div> 
+        <div className={stylesCard.card_subtitle}>
+          <img src={iconePredio} alt="Ícone de hotel"/>
+          <p>{subtitulo}</p>
         </div>
-        <div className={stylesCard.card_info}>
-          <h4>{titulo}</h4>
-          <div className={stylesCard.card_subtitle}>
-            <i>{iconePredio}</i>
-            <p>{subtitulo}</p>
-          </div>
-          <div className={stylesCard.card_pontuacao}>
-            <i>{iconeEstrela}</i>
-            <span>{pontuacao}</span>
-            <span>
-              (<a>Reviews</a>)
-            </span>
-          </div>
-          <div className={stylesCard.card_options}>
-            <a>{iconeAdicionar}</a>
-            <i></i>
-            <i></i>
-          </div>
-          <div className={stylesCard.card_valor}>{valor}</div>
+        <div className={stylesCard.card_pontuacao}>
+          <img src={iconeEstrela} />
+          <span>{pontuacao}</span>
+          <span>
+            (<a>Reviews</a>)
+          </span>
         </div>
+        <div className={stylesCard.card_options}>
+          <a>{iconeAdicionar}</a>
+          <i></i>
+          <i></i>
+        </div>
+        <div className={stylesCard.card_valor}>{valor}</div>
       </div>
     </div>
   );
