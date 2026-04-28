bca6105f002e9d6d8f43e485c5c7b0a34a8b5989
Pedro Mota Maia <pedro.mmaia@al.infnet.edu.br>
2026-04-28 08:14:22 -0300

    reorganização e introdução do card

--- app-viagens/src/pages/home/main_page.jsx
+++ app-viagens/src/pages/home/main_page.jsx
@@ -1,11 +1,23 @@
 import "./main_page.css"
-
+import Card from "../../components/Card/Card.jsx"
 function Home() {
     return (
         <>
             <div className="tela">
                 <div className="foto_background"></div>
-                <div className="painel_verde"></div>
+                <div className="painel_verde">
+                    <Card
+                        imagem="Munique.jpg"
+                        titulo="Munique, Alemanha"
+                        iconePredio="icone-hotel.png"
+                        subtitulo="Munich Marriott Hotel"
+                        iconeEstrela={"icone-estrela.png"}
+                        pontuacao={4.8}
+                        iconeAdicionar={"icone-plus.png"}
+                        iconeCar={"icone-car.png"}
+                        iconePlaca={"icone-placa.png"}
+                        valor={"R$2.458"} />
+                </div>
             </div>
         </>
     );
