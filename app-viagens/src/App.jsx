abe9f5149bc6cfe4e7e4969b286f3da16605a7f3
Claudia Santos Silva <claudia.tss@outlook.com>
2026-05-01 18:32:53 -0300

    Criada pasta CardPassagem para seus componentes.

--- app-viagens/src/App.jsx
+++ app-viagens/src/App.jsx
@@ -1,11 +1,9 @@
 import Home from "./pages/home/main_page";
-import CardPassagem from "./components/CardPassagem";
 
 function App() {
   return (
     <>
       <Home />
-      <CardPassagem />
     </>
   );
 }
