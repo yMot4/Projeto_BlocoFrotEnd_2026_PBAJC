f701169daf747408e07096c178d2592f03323870
pedro mota <pmaia2004@gmail.com>
2026-05-07 15:35:14 -0300

    style(page): Estruturação da terceira página semi-concluída, início da section de opcionais(hotel, carro, ect).

--- app-viagens/src/App.jsx
+++ app-viagens/src/App.jsx
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/main_page";
 import Passagens from "./pages/Passagens/Passagens";
+import Detalhes from "./pages/Detalhes/Detalhes";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/passagens" element={<Passagens />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <Detalhes/>
     </>
   );
 }
