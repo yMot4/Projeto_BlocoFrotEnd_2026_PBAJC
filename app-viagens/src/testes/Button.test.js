589576191181ba3497ed1f4cbdab573715914947
Claudia Santos Silva <claudia.tss@outlook.com>
2026-05-22 20:13:27 -0300

    Button.test estruturado

---
+++ app-viagens/src/testes/Button.test.js
@@ -0,0 +1,17 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Button from "../components/Button";
+
+test("renderiza botão", ()=>{
+    render(COLOCAR O BUTTON INTEIRO AQUI);
+    expect(screen.getByRole("button", {name: /voltar/i})).toBeInTheDocument();
+});
+
+test("executa clique", async()=>{
+    const mockFn = jest.fn();
+    render (BOTAO INTEIRO COM NAME E ONCLICK);
+    const botao = screen.getByRole("button");
+    await userEvent.click(botao);
+    expect(mockFn).toHaveBeenCalled();
+});
