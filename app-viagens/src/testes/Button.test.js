979d2d4c27d9c66afebf233027e96511224855f4
Claudia Santos Silva <claudia.tss@outlook.com>
2026-05-23 22:07:45 -0300

    Button.test preenchido com o componente Button.

--- app-viagens/src/testes/Button.test.js
+++ app-viagens/src/testes/Button.test.js
@@ -3,15 +3,23 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Button from "../components/Button";
 
-test("renderiza botão", ()=>{
-    render(COLOCAR O BUTTON INTEIRO AQUI);
+test("renderiza botão com icone", ()=>{
+    render(
+        <Button ariaLabel="Voltar" >
+            <ArrowRightStroke rotate={180} size="md" color="white" />
+        </Button> 
+    );
     expect(screen.getByRole("button", {name: /voltar/i})).toBeInTheDocument(); //regex e o i faz ignorar maiusculas e minusculas
 });
 
 test("executa clique", async()=>{
     const mockFn = jest.fn();
-    render (BOTAO INTEIRO COM NAME E ONCLICK);
-    const botao = screen.getByRole("button");
+    render (
+        <Button ariaLabel="Voltar" onClick={mockFn}>
+            <ArrowRightStroke rotate={180} size="md" color="white" />
+        </Button> 
+    );
+    const botao = screen.getByRole("button",{name: /voltar/i});
     await userEvent.click(botao);
     expect(mockFn).toHaveBeenCalled();
 });
